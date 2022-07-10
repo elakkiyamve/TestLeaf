c886ae5b476b9981852ce99689e61fd3b31bcf62
Elakkiya aravind <94050446+elakkiyamve@users.noreply.github.com>
2022-07-10 11:22:53 +0530

    week1day1

---
+++ CondStatement.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class CondStatement {
+	public static void main(String[] args) {
+		
+		// for (int i =1; i<20; i++) 
+		{
+			int i=8;
+			if( i % 2 == 0 ) {
+				System.out.println("even number");
+				
+			}else {
+				System.out.println("odd number");
+
+			}
+		}
+	}
+
+}
